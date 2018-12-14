0f2bdb89289f6982d45f17eafd12bab7f24252fb
Nicholas Humes <njhumes@gmail.com>
2018-12-14 12:59:22 -0700

    The Farm

--- js/app.js
+++ js/app.js
@@ -39,3 +39,12 @@ console.log(e == 'Kevin');
 console.log(a < b + c);
 console.log(a != a + d);
 console.log(48 == '48');
+
+// D. The Farm
+
+let animal = 'monkey';
+if(animal == 'cow') {
+    console.log('mooooo');
+} else {
+        console.log('Hey! You\'re not a cow.')
+    }
