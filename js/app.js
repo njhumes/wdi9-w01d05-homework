0a943946af0cba8f58154a80b7f0af0415f74056
Nicholas Humes <njhumes@gmail.com>
2018-12-14 12:50:11 -0700

    Q&A and Strings Completed

---
+++ js/app.js
@@ -0,0 +1,23 @@
+console.log('Day5/Weekend Homework')
+
+/* I. Variables & Datatypes
+A. Q & A
+1.  let or const variableName = value
+2.  varName = newvalue if used let to declear originally. If used const, we can't change 
+3.  let newVar = existVar
+4.  Declare is when you first introduce the variable. You need to declare it first before you can assign anythign to it
+    Assign and Define just mean to give that variable some kind of value
+5.  Pseudocoding is writing out the steps you need to take in plain english in order to solve the problem
+6. about 75% planning, 25% writing code
+
+B. Strings
+*/
+let firstVariable = 'Hello World';
+firstVariable = 33;
+let secondVariable = firstVariable;
+secondVariable = 'Any string';
+console.log(firstVariable); // will return 33;
+console.log(secondVariable);
+
+let yourName = 'Nick Humes';
+console.log(`Hello, my name is ${yourName}`);
