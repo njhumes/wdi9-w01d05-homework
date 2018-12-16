838c2a0b765652ce6c8edaf7b5ebe911d7c0dae6
Nicholas Humes <njhumes@gmail.com>
2018-12-16 14:27:38 -0700

    users loops

--- js/app.js
+++ js/app.js
@@ -338,3 +338,10 @@ user.friend.purchased.push('The One Ring');
 user.friend.purchased.push('A latte');
 console.log(user.friend.purchased[1]);
 console.log(user);
+
+for (let i = 0; i < user.purchased.length; i++){
+    console.log(user.purchased[i]);
+}
+for (let i = 0; i < user.friend.purchased.length; i++){
+    console.log(user.friend.purchased[i]);
+}
