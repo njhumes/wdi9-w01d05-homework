44f16773a7c6307df97f97a9fe80c5b6055f8a0d
Nicholas Humes <njhumes@gmail.com>
2018-12-15 21:03:09 -0700

    Whats in your closet

--- js/app.js
+++ js/app.js
@@ -134,7 +134,7 @@ myArray.unshift('Bob Marley');
 myArray.pop();
 myArray.reverse();
 console.log(myArray);
-let myInt = 33;
+let myInt = 7;
 if(myInt > 10){
     console.log('big number');
 } else if (myInt < 5) {
@@ -142,3 +142,43 @@ if(myInt > 10){
 } else {
     console.log('monkey')
 }
+
+
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+    [
+        // These are Thom's shirts
+        "grey button-up",
+        "dark grey button-up",
+        "light blue button-up",
+        "blue button-up",
+    ], [
+        // These are Thom's pants
+        "grey jeans",
+        "jeans",
+        "PJs"
+    ], [
+        // Thom's accessories
+        "wool mittens",
+        "wool scarf",
+        "raybans"
+    ]
+];
+
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`)
+kristynsCloset.splice(6, 0, 'raybans');
+console.log(kristynsCloset);
+kristynsCloset[5] = 'stained knit hat'
+console.log(kristynsCloset);
+console.log(`Thom is looking fierce in his ${thomsCloset[0][0]}, ${thomsCloset[1][1]}, and ${thomsCloset[2][1]}`);
